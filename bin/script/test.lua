b84dbc39a5b9dbede7c6fa7692e5229b62a24e14
brinkqiang <brink.qiang@gmail.com>
2019-05-22 15:31:58 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -1,11 +1,11 @@
 
 local redis = require "luaredis"
-local HOST = "192.168.1.33"
+local HOST = "127.0.0.1"
 local PORT = 6379
 
 local conn = redis.connect(HOST, PORT)
 local info = redis.unwrap_reply(conn:command("INFO"))
 print(info)
-conn:command("SET", "MYKEY", "MYVALUE")
+conn:command("SET", "MYKEY", "hello world!")
 local value = conn:command("GET", "MYKEY")
 print(value)
