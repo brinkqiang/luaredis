63effa03cecb2ea9a0075a40c4ca86a720b5f562
brinkqiang <brink.qiang@gmail.com>
2019-05-22 15:23:22 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -6,3 +6,6 @@ local PORT = 6379
 local conn = redis.connect(HOST, PORT)
 local info = redis.unwrap_reply(conn:command("INFO"))
 print(info)
+conn:command("SET", "MYKEY", "MYVALUE")
+local value = conn:command("GET", "MYKEY")
+print(value)
