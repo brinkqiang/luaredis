cbcd63546c22f39a66330536025237567f0dee50
brinkqiang <brink.qiang@gmail.com>
2019-05-27 11:42:05 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -4,6 +4,12 @@ local HOST = "127.0.0.1"
 local PORT = 6379
 
 local conn = redis.connect(HOST, PORT)
+local ok = conn:command("AUTH", "123456")
+print(ok)
+if ok ~= "OK"
+    return
+end
+
 local info = redis.unwrap_reply(conn:command("INFO"))
 print(info)
 conn:command("SET", "MYKEY", "hello world!")
